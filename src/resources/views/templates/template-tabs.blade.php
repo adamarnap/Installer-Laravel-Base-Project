cbcd417cf549c35e93e83fe646390f277b5d3957
adamarnap <adamarnap0117@gmail.com>
2026-07-29 14:24:44 +0700

    Give template html for tabs

---
+++ src/resources/views/templates/template-tabs.blade.php
@@ -0,0 +1,29 @@
+<div class="card border-borderColor rounded-[5px] shadow-xs bg-white mb-6">
+    <div class="card-header border-borderColor p-4 border-b">
+        <h5 class="card-title">Justified Nav Tabs</h5>
+    </div>
+    <div class="card-body p-4">
+        <ul class="flex flex-wrap justify-between text-sm font-medium text-center mb-3" id="default-styled-tab12" data-tabs-toggle="#company-tab-content12" data-tabs-active-classes="text-white hover:text-white bg-primary" data-tabs-inactive-classes="text-gray-500 hover:text-white border-transparent hover:bg-primary" role="tablist">
+            <li class="me-8 flex-1" role="presentation">
+                <button class="inline-block w-full rounded py-2 px-4 " data-tabs-target="#company-basic12" type="button" role="tab" aria-selected="false">Home</button>
+            </li>
+            <li class="me-6 flex-1" role="presentation">
+                <button class="inline-block w-full rounded py-2 px-4 " data-tabs-target="#company-address12" type="button" role="tab" aria-selected="false">About</button>
+            </li>
+            <li class="me-6 flex-1" role="presentation">
+                <button class="inline-block w-full rounded py-2 px-4 " data-tabs-target="#company-social15" type="button" role="tab" aria-selected="false">Services</button>
+            </li>
+        </ul>
+        <div id="company-tab-content12">
+            <div class="hidden " id="company-basic12" role="tabpanel">
+                {{-- Start Content For this tab --}}
+            </div>
+            <div class="hidden" id="company-address12" role="tabpanel">
+                {{-- Start Content For this tab --}}
+            </div>
+            <div class="hidden" id="company-social15" role="tabpanel">
+                {{-- Start Content For this tab --}}
+            </div>
+        </div>
+    </div>
+</div>
